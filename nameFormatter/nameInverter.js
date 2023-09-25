71874176317cd6525f39858e2af4891804b26a18
Todd <todd.fountain39@gmail.com>
2023-09-25 15:20:08 -0500

    Name inverter

---
+++ nameFormatter/nameInverter.js
@@ -0,0 +1,23 @@
+// return empty string when passed empty string
+// return a single name when passed single name
+// return single name when passed single name with extra spaces
+// return last-this.name, first-name when passed a first and last-this.name
+// return last name, first name when passed first and last with eextra spaces
+// return empty with passed single honorific
+// return honorific first name when passed honorific first name
+// return honorific last-name, first-name when passed honorific first-name last-name with extra spaces around the words
+// throw an error when name is undefined
+
+
+const nameInverter = function(name) {
+  
+  
+  if (name === '') {
+    return '';
+  } if (name === '') {
+    return ''
+  } 
+return name;
+};
+
+module.exports = nameInverter;
